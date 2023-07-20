b4b0cc81d5d0fddd6493dfa921c56d7c74b67699
Aishwarya K <aishusindhu95@gmail.com>
2023-07-20 10:05:10 +0530

    karthi branch

--- src/org/cts/GitFirstSpotify.java
+++ src/org/cts/GitFirstSpotify.java
@@ -1,5 +1,12 @@
 package org.cts;
 
 public class GitFirstSpotify {
-
+	 
+	public static void main(String[] args) {
+		System.out.println("Spotify App");
+	}
+	
+	private void name() {
+		System.out.println("This is karthi Branch ");
+	}
 }
